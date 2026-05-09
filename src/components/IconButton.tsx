4b51377994781dd4eaa47d6f823e2065edcbcf9b
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-09 12:28:26 +0200

    Better settings icon colors

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -7,7 +7,7 @@ interface IconButtonProps {
 
 function IconButton({ children, className="" }: IconButtonProps) {
     return (
-        <div className={`aspect-square h-full bg-white/20 rounded-xl border border-white/20 flex flex-row items-center justify-center ${className} transition-all duration-300 hover:bg-white/30 hover:border-white/30 hover:text-white hover:scale-103`}>
+        <div className={`aspect-square h-full bg-white/20 rounded-xl border border-white/20 flex flex-row items-center justify-center ${className} transition-all duration-300 hover:bg-white/30 hover:border-white/30 hover:text-white hover:scale-103 active:bg-white/15 active:border-white/15 active:text-neutral-400 active:scale-97`}>
             {children}
         </div>
     );
