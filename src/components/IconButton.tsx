04ff9c97a06fb6ee08c5baa13310fd8b022d2858
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-09 12:12:21 +0200

    Added scale animation when hover on IconButton

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -7,7 +7,7 @@ interface IconButtonProps {
 
 function IconButton({ children, className="" }: IconButtonProps) {
     return (
-        <div className={`aspect-square h-full bg-white/20 rounded-xl border border-white/20 flex flex-row items-center justify-center ${className} transition-all duration-300 hover:bg-white/30 hover:border-white/30 hover:text-white`}>
+        <div className={`aspect-square h-full bg-white/20 rounded-xl border border-white/20 flex flex-row items-center justify-center ${className} transition-all duration-300 hover:bg-white/30 hover:border-white/30 hover:text-white hover:scale-103`}>
             {children}
         </div>
     );
