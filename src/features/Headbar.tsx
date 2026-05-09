047e013f3499709ca7079e75857e36c0e2d6b2fc
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-09 08:46:09 +0200

    Changed Nunito font with ElmsSans

--- src/features/Headbar.tsx
+++ src/features/Headbar.tsx
@@ -1,7 +1,7 @@
 function Headbar() {
     return (
         <div className={`w-screen h-[10%] flex flex-row items-center justify-between p-5`}>
-            <div className={`font-nunito text-white text-3xl font-bold select-none`}>
+            <div className={`font-elms text-white text-3xl font-bold select-none`}>
                 Lucid
             </div>
         </div>
