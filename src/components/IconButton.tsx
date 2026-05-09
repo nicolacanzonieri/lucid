fae33ddc2884940e6d51f8c41a3d643501081fe5
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-09 11:30:44 +0200

    Added className to IconButton to better manage the style of the component

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from 'react';
 
 interface IconButtonProps {
     children: ReactNode;
+    className?: string;
 }
 
-function IconButton({ children }: IconButtonProps) {
+function IconButton({ children, className="" }: IconButtonProps) {
     return (
-        <div className={`aspect-square h-full rounded-md bg-white/35 flex flex-row items-center justify-center`}>
+        <div className={`aspect-square h-full rounded-xl bg-white/35 flex flex-row items-center justify-center ${className}`}>
             {children}
         </div>
     );
