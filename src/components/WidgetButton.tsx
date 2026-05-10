db785fcfaaa3172a65a5e93af2ac8b4fe5b7cdea
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-10 11:18:45 +0200

    Started working on widget button

---
+++ src/components/WidgetButton.tsx
@@ -0,0 +1,11 @@
+function WidgetButton() {
+    return (
+        <div className={`w-50 h-full pt-3`}>
+            <div className={`w-full h-full rounded-t-lg bg-white/20`}>
+
+            </div>
+        </div>
+    )
+}
+
+export default WidgetButton;
--- src/features/BottomBar.tsx
+++ src/features/BottomBar.tsx
@@ -1,6 +1,9 @@
+import WidgetButton from "../components/WidgetButton";
+
 function BottomBar() {
     return (
-        <div className={`w-full h-10 flex flex-row items-center justify-between p-4`}>
+        <div className={`w-full h-10 flex flex-row items-center justify-center p-0`}>
+            <WidgetButton />
         </div>
     );
 }
