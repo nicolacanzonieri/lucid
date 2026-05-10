5324c14554d4e4b478beb8c825ca3eb78843c336
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-10 11:14:56 +0200

    Moved iconbutton svg classNames to the button itself

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -7,8 +7,10 @@ interface IconButtonProps {
 
 function IconButton({ children, className="" }: IconButtonProps) {
     return (
-        <div className={`aspect-square h-full bg-white/20 rounded-xl border border-white/15 flex flex-row items-center justify-center ${className} transition-all ease-in-out duration-300 hover:bg-white/30 hover:border-white/25 hover:text-white hover:scale-103 active:bg-white/15 active:border-white/10 active:text-neutral-400 active:scale-97`}>
-            {children}
+        <div className={`aspect-square h-full bg-white/15 rounded-xl border border-white/15 flex flex-row items-center justify-center ${className} transition-all ease-in-out duration-300 hover:bg-white/30 hover:border-white/25 hover:text-white hover:scale-103 active:bg-white/10 active:border-white/10 active:text-neutral-400 active:scale-97`}>
+            <span className="aspect-square w-full p-2 block">
+                {children}
+            </span>
         </div>
     );
 }
