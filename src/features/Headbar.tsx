693bd1893bad8746648fa545f60a8f05f389a0b1
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-13 09:39:49 +0200

    Updated button styles and header layout. Defined CSS variables for button theme consistency. Refactored IconButton to use theme variables, removing the className prop. Updated Headbar layout: add welcome message and hide title.

--- src/features/Headbar.tsx
+++ src/features/Headbar.tsx
@@ -39,17 +39,22 @@ function Headbar() {
     return (
         <div className={`w-full h-17 flex flex-row items-center justify-between p-3`}>
             {/* Title */}
-            <div className={`font-elms text-3xl font-normal select-none transition-all duration-500 ease-in-out ${isUserIdle ? "text-transparent" : "text-white"}`}>
+            {/* <div className={`font-elms text-3xl font-normal select-none transition-all duration-500 ease-in-out ${isUserIdle ? "text-transparent" : "text-white"}`}>
                 Lucid
-            </div>
+            </div> */}
 
             {/* TIME */}
-            <div className={`font-elms text-neutral-300 text-md font-normal select-none`}>
-                {`${dayName} ${dayNumber} ${monthName} - ${time}`}
+            <div className={`h-full flex flex-col items-start`}>
+                <span className={`font-elms text-neutral-300 text-md font-bold select-none`}>
+                    {`${time} ${dayName} ${dayNumber} ${monthName}`}
+                </span>
+                <span className={`font-elms text-neutral-300 text-md font-light select-none`}>
+                    Welcome to Lucid, Nico
+                </span>
             </div>
 
             {/* Settings */}
-            <IconButton className={`${isUserIdle ? `text-transparent` : `text-neutral-300`}`} isUserIdle={isUserIdle}>
+            <IconButton isUserIdle={isUserIdle}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M11 10.27 7 3.34"/><path d="m11 13.73-4 6.93"/><path d="M12 22v-2"/><path d="M12 2v2"/><path d="M14 12h8"/><path d="m17 20.66-1-1.73"/><path d="m17 3.34-1 1.73"/><path d="M2 12h2"/><path d="m20.66 17-1.73-1"/><path d="m20.66 7-1.73 1"/><path d="m3.34 17 1.73-1"/><path d="m3.34 7 1.73 1"/><circle cx="12" cy="12" r="2"/><circle cx="12" cy="12" r="8"/></svg>
             </IconButton>
         </div>
