f295fbef7122ddc0bc7a2088f6021973c5bd70ef
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-15 09:34:31 +0200

    Fixed modal closing system

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 function Modal() {
     return (
-        <div className="w-[70%] h-[70%] bg-btn-bg-1 rounded-3xl border-btn-border-1 border-t border-b-[0.5px]">
+        <div onClick={(e) => {e.stopPropagation()}} className="w-[70%] h-[70%] bg-btn-bg-1 rounded-3xl border-btn-border-1 border-t border-b-[0.5px]">
           
         </div>
     );
