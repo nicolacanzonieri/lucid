da97b05640b2c772fa31ccc0afedd48264cc8b5e
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-15 16:17:31 +0200

    Added backdrop blur to icon button and widget button

--- src/components/WidgetButton.tsx
+++ src/components/WidgetButton.tsx
@@ -5,7 +5,7 @@ interface WidgetButtonPros {
 function WidgetButton({ isUserIdle=false }: WidgetButtonPros) {
     return (
         <div className={`w-50 h-full pt-p-1`}>
-            <div className={`w-full h-full flex flex-col items-center justify-center rounded-t-xl border-t ${isUserIdle ? `bg-transparent border-transparent text-transparent` : `bg-btn-bg-1 border-btn-border-1 text-btn-text-1`} transition-all ease-in-out ${isUserIdle ? `duration-1000` : `duration-150`} hover:bg-btn-hover-bg-1 hover:border-btn-hover-border-1 hover:text-btn-hover-text-1 hover:scale-101 active:bg-btn-active-bg-1 active:border-btn-active-border-1 active:border-t-0 active:border-l-[0.5px] active:border-r-[0.5px] active:text-btn-active-text-1 active:scale-97`}>
+            <div className={`w-full h-full flex flex-col items-center justify-center rounded-t-xl border-t ${isUserIdle ? `bg-transparent border-transparent text-transparent backdrop-blur-none` : `bg-btn-bg-1 border-btn-border-1 text-btn-text-1 backdrop-blur-xl`} transition-all ease-in-out ${isUserIdle ? `duration-1000` : `duration-150`} hover:bg-btn-hover-bg-1 hover:border-btn-hover-border-1 hover:text-btn-hover-text-1 hover:scale-101 active:bg-btn-active-bg-1 active:border-btn-active-border-1 active:border-t-0 active:border-l-[0.5px] active:border-r-[0.5px] active:text-btn-active-text-1 active:scale-97`}>
                 {/* <span className={`text-xs uppercase font-extralight select-none`}>
                     Widgets
                 </span> */}
