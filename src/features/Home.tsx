429915ef4ec075bfb9c87107aabedc1b8dbd4875
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-05-16 21:57:51 +0200

    Added drop shadow effect to lucid text in home

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <div className={`font-bumbbled text-white text-center text-7xl select-none`}>
+        <div className={`p-8 font-bumbbled text-white text-center text-7xl select-none filter drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]`}>
             Lucid
         </div>
     );
